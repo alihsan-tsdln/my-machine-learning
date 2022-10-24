da8393fd4091c136cf860c3a96f11880f264a0b6
alihsan-tsdln <alihsan.tsdln@gmail.com>
2022-10-24 23:42:09 +0300

    Polynomial Linear Regression Python Code

--- Course 4 - Polynomial Linear Regression/polynomialLinearRegression.py
+++ Course 4 - Polynomial Linear Regression/polynomialLinearRegression.py
@@ -1,66 +1,22 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 24 18:56:27 2022
-
-@author: alihsan-tsdln
-"""
-
 import pandas as pd
-
 data = pd.read_csv("maaslar.csv")
 
-x = data.iloc[:, 1:2]
-y = data.iloc[:, 2:3]
+input_data = data.iloc[:, 1:2]
+output_data = data.iloc[:, 2:]
 
-#with linear regression
 import matplotlib.pyplot as plt
-plt.scatter(x.values,y.values, color="red")
+plt.scatter(input_data, output_data, color = "blue")
 
 from sklearn.linear_model import LinearRegression
 
-guess = LinearRegression().fit(x.values, y.values)
-guess = guess.predict(x.values)
-
-plt.plot(x.values, guess)
-plt.scatter(x.values, y.values, color="green")
-
-#with polynomial linear regression
 from sklearn.preprocessing import PolynomialFeatures
+import numpy as np
 
-pf2 = PolynomialFeatures(degree = 2)
-x_poly = pf2.fit_transform(x.values)
+colors = np.array(["grey", "green", "purple", "red", "orange", "brown", "black"])
 
-guess_poly = LinearRegression().fit(x_poly, y.values)
-guess_poly = guess_poly.predict(x_poly)
-
-plt.plot(x.values, guess_poly, color="red")
-
-pf3 = PolynomialFeatures(degree = 3)
-x_poly = pf3.fit_transform(x.values)
-
-guess_poly = LinearRegression().fit(x_poly, y.values)
-guess_poly = guess_poly.predict(x_poly)
-
-plt.plot(x.values, guess_poly, color="blue")
-
-pf4 = PolynomialFeatures(degree = 4)
-x_poly = pf4.fit_transform(x.values)
-
-guess_poly = LinearRegression().fit(x_poly, y.values)
-guess_poly = guess_poly.predict(x_poly)
-
-plt.plot(x.values, guess_poly, color="purple")
-
-pf5 = PolynomialFeatures(degree = 5)
-x_poly = pf5.fit_transform(x.values)
-
-guess_poly = LinearRegression().fit(x_poly, y.values)
-guess_poly = guess_poly.predict(x_poly)
-
-plt.plot(x.values, guess_poly, color="orange")
-
-#most powerful predicts with 4 and numbers which bigger than 4 
-
-
-
+for i in range(1,7):
+    pf = PolynomialFeatures(degree = i).fit_transform(input_data)
+    a = predictDeg = LinearRegression().fit(pf, output_data)
+    predictDeg = LinearRegression().fit(pf, output_data).predict(pf)
+    plt.plot(input_data,predictDeg, color = colors[i])
 
